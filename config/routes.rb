fc5603fab8f0246e82f6e49ea23e975e8a165371
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-02 15:35:05 -0500

    defined user routes
    defined routes for user meals, cuisines

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:show, :create, :update] do
+    resources :meals, only: [:index, :create]
+  end
+  resources :cuisines, only: [:show, :create, :index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
