acd5d59fb4f9617d43b105bce7f593a66a96b3be
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-02 14:23:59 -0500

    updated spec helper /Database Cleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
   # rspec-expectations config goes here. You can use an alternate
