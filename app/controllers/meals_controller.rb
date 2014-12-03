d0b6f74e11f51669e07b70dac251f7690c57a6d6
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-03 12:41:15 -0500

    added explicit renders to meals and users
    filled out users controller
    
    filled out meals controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,18 @@
 class MealsController < ApplicationController
   def index
+    @meals = Meal.find_by(user_id: params[:user_id])
+    render
   end
 
   def create
+    @meal = Meal.create(meal_params)
+    render
   end
+
+  private
+
+    def meal_params
+      params.require(:meal).permit(:user_id, :cuisine_id)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    render
   end
 
   def create
+    @user = User.create(user_params)
+    render
   end
 
   def update
+    @user = User.update(user_params)
+    render
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+
 end
